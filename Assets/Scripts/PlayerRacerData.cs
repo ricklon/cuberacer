1b955395a9259e8564bf0e7b58d82226a03526ff
Rick Anderson <rick.rickanderson@gmail.com>
2018-01-28 17:43:28 -0500

    cubelabd scene

---
+++ Assets/Scripts/PlayerRacerData.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRacerData : MonoBehaviour {
+
+  public Rigidbody rb;
+  //float speed = 0.0f;
+  //float rotationalSpeed = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("speed: " + rb.velocity.magnitude);
+		Debug.Log("Angle: " + transform.eulerAngles.y   );
+		 
+	}
+}
